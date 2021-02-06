9f05bece806f043b6d261702ffec726a720637b5
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-06 14:45:29 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int
+main(void)
+{
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://netio-4c.netio-products.com:8080/netio.json");
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
+		struct curl_slist *headers = NULL;
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		const char *data = "{\"Outputs\": [{\"ID\": 1,\"Action\": 2,\"Delay\": 10000},{\"ID\": 2,\"Action\": 2,\"Delay\": 12000},{\"ID\": 3,\"Action\": 2,\"Delay\": 14000},{\"ID\": 4,\"Action\": 2,\"Delay\": 16000}]\n}";
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+		res = curl_easy_perform(curl);
+	}
+curl_easy_cleanup(curl);
+}
