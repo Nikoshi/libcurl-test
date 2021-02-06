facc130580f1749df77cbc6bec1688253499c7f0
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-06 16:30:44 +0100

    Print response error

--- main.c
+++ main.c
@@ -43,6 +43,8 @@ main(int argc, char **argv)
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
 		res = curl_easy_perform(curl);
+		if(res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		
 		free(headers);
 		curl_easy_cleanup(curl);
