7b59768fae0f228afb46efd70efb76b2991d777c
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-06 15:57:54 +0100

    Suppress body output

--- simple.c
+++ simple.c
@@ -3,8 +3,9 @@
 #include <curl/curl.h>
 
 int
-main(int argc, char **argv)
+main(void)
 {
+	FILE *null_file = fopen("/dev/null", "w");
 	CURL *curl;
 	CURLcode res;
 	curl = curl_easy_init();
@@ -13,6 +14,7 @@ main(int argc, char **argv)
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_easy_setopt(curl, CURLOPT_URL, "http://netio-4c.netio-products.com:8080/netio.json");
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, null_file);
 
 		struct curl_slist *headers = NULL;
 		headers = curl_slist_append(headers, "Content-Type: application/json");
@@ -22,7 +24,8 @@ main(int argc, char **argv)
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
 		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
 	}
-
-	curl_easy_cleanup(curl);
+	if (null_file)
+		fclose(null_file);
 }
